56023490c8cd57febe408621ee808ee59e9239ea
patrickxu@wiatec.com <patrickxu@wiatec.com>
2018-11-24 11:49:33 +0800

    201811241148

---
+++ lib/data/ufc_provider.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+
+
+
+
+class UfcProvider{
+
+  Dio dio = Dio();
+  final String EVENT_URL = "http://ufc-data-api.ufc.com/api/v1/us/events";
+  final String MEDIA_URL = "http://ufc-data-api.ufc.com/api/v1/us/media/665797";
+
+
+  Future<String> fetchEvents () async{
+    Response response = await dio.get(EVENT_URL).catchError((DioError e){
+      print("DioError: " + e.toString());
+    });
+    if(response.statusCode == 200){
+      List dataList = response.data;
+      print(dataList);
+
+    }
+    return "";
+  }
+}
